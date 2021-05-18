95f221597730f19f4150672b68190e4f4644b810
Rokot2109 <77354426+Rokot2109@users.noreply.github.com>
2021-05-18 10:05:15 +0300

    Правки, калькуляторы в корне в отдельной папке

---
+++ db.php
@@ -0,0 +1,2 @@
+<?php
+$db = @mysqli_connect("localhost:3306", "mysql", "mysql", "geek" ) or Die(mysqli_connect_erorr());
---
+++ menu.php
@@ -0,0 +1,3 @@
+<a href="/">Галерея</a>
+<a href="/catalog.php">Каталог</a>
+<a href="/feedback.php">Отзывы</a>
